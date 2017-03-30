f0339d0c693bdb2bd76fc8d8c935944cd01d2f7d
Felipe <info@icologic.co>
2017-03-30 15:51:46 +0200

    Ajustes_incluyendo workspace _ 300317 _ 1551

---
+++ App_CMDB/API_UTILS.swift
@@ -0,0 +1,22 @@
+//
+//  API_UTILS.swift
+//  App_CMDB
+//
+//  Created by Andres on 30/3/17.
+//  Copyright © 2017 icologic. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+let CONSTANTES = Constantes()
+
+
+struct Constantes {
+    let COLORES_BASE = Colores()
+}
+
+struct Colores {
+    let COLOR_GRIS_TAB_NAV_BAR = #colorLiteral(red: 0.2, green: 0.2, blue: 0.2, alpha: 1)
+    let COLOR_BLANCO_GENERAL = #colorLiteral(red: 0.9058823529, green: 0.2980392157, blue: 0.2352941176, alpha: 1)
+}
---
+++ Pods/Target Support Files/Pods-App_CMDB/Pods-App_CMDB-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_App_CMDB : NSObject
+@end
+@implementation PodsDummy_Pods_App_CMDB
+@end
